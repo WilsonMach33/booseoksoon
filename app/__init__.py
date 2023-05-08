0b7b1c07c4fcc816c7c8da458398974120eecaa2
Jasmine Yuen <jyuen30@stuy.edu>
2023-05-08 09:21:56 -0400

    fixed dictionary

--- app/__init__.py
+++ app/__init__.py
@@ -97,14 +97,17 @@ def user_stats_page():
 # modeling .csv data
 def model_all():
     dataDict = {}
-    dataDict["name"] = []
-    dataDict["album"] = []
+    #dataDict["name"] = []
+    #dataDict["album"] = []
     with open('spotify_taylorswift.csv') as f:
         r = csv.DictReader(f)
         for row in r:
-            dataDict["name"].append(row["name"])
-            dataDict["album"].append(row["album"])
-        return dataDict
+            albumName = row["album"]
+            if (albumName not in dataDict):
+                dataDict[albumName] = []
+            dataDict[albumName].append(row["name"])
+    print(dataDict)
+    return dataDict
 
 if __name__ == "__main__":
     app.debug = True
